3d9eff42545e4be86cc8fadc9780d551c69ba6b1
josephineschlegel <josephine.jschlegel@gmail.com>
2020-05-19 17:00:15 +0200

    updating from protoype

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+//STICKY NAV BAR//
+window.onscroll = function() {myFunction()};
 
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
